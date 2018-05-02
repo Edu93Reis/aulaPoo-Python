00f3e1a41010b9e14b103fcf23afbe8ce7a14690
edu93reis <eduardo.reis93@gmail.com>
2018-05-02 00:02:27 -0300

    Ex3.1

---
+++ T3/T1/Teste.py
@@ -0,0 +1,12 @@
+from Biblioteca import Biblioteca
+from Livro import Livro
+from Estante import Estante
+from Categoria import Categoria
+from Menu import Menu
+
+class Teste:
+    m = Menu()
+
+    m.menu()
+
+Teste()
