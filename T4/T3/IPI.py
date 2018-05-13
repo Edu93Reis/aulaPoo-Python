025fa487e5457ad18e1c22d5bc1251f0927eb52d
edu93reis <eduardo.reis93@gmail.com>
2018-05-12 22:39:44 -0300

    Ex 3,4 finais

--- T4/T3/IPI.py
+++ T4/T3/IPI.py
@@ -3,5 +3,5 @@ from Imposto import Imposto
 class IPI(Imposto):
 
     def __init__(self):
-        self._aliquota = _aliquota = 0.90
+        self._aliquota = _aliquota = 0.95
         super(IPI, self).__init__(_aliquota)
--- T4/T3/Produto.py
+++ T4/T3/Produto.py
@@ -8,7 +8,7 @@ class Produto:
         self.__imposto = __imposto
 
     def getPreco(self):
-        return self.__preco * self.__imposto.aliquota
+        return self.__preco * self.__imposto._aliquota
 
     def getNome(self):
         return self.__nome
